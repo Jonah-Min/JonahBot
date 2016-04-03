9e1d92138932363a9c70e6ae7086e659ec987912
Jonah-Min <jonahm2472@gmail.com>
2016-04-03 17:23:10 -0400

    MAL and champion.gg
    Added My Anime List manga and anime API searching. Also added a html
    scraper to get champion winrates and play rates from champion.gg

--- jonahbot.rb
+++ jonahbot.rb
@@ -3,21 +3,36 @@
 require 'discordrb'
 require 'open-uri'
 require 'json'
+require 'nokogiri'
 
 # creates new random number generator
 r = Random.new
 
 # initialize bot
-bot = Discordrb::Commands::CommandBot.new 'email@website.com', 'password', '!'
+# in order to start the bot, it requries a discord account
+# the first argument is the email used to create the account
+bot = Discordrb::Commands::CommandBot.new 'username@email.com', 'password', '!'
 
 # grab all champions from league of legends api for league commands
-html = open('https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key= Get Your Own at developer.riotgames.com', 'User-Agent' => 'Ruby').read
+# Requires account on riot games as well as an api key found at developer.riotgames.com
+html = open('https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key= GET YOURS AT developer.riotgames.com', 'User-Agent' => 'Ruby').read
 
 response = JSON.parse(html)
 response = response["data"]
 champs = []
 
 response.each { |champ, value| champs.push(champ) }
+
+# Checks for new members joinging the discord channel
+bot.member_join do |event|
+	event.server.general_channel.send_message("#{:member} joined this server!")
+end
+
+# Once it can take commands it will print message to send to channel
+# Requires discord's code for your channel
+bot.ready do 
+	bot.send_message('channel code', "\n Jonah Bot is up and running! \n\n Type !commands for a list of available commands  ^ ^")
+end
 
 # Says hello to the user 
 bot.command :hi do |event|
@@ -29,7 +44,6 @@ bot.command(:randomroles, min_args: 2, max_args: 5) do |_event, *args|
 	
 	roles = ['Top:   ', 'Jung:  ', 'Mid:   ', 'ADC:  ', 'Supp:  ']
 	roles.shuffle!
-
 	args.shuffle!
 
 	size = args.size()
@@ -70,11 +84,13 @@ bot.command(:reddit, min_args: 3, max_args: 3) do |_event, *args|
 	category = args[1]
 	number = args[2].to_i
 
+	# Only parses first page of results
 	if number > 25 || number < 1
 		_event.respond "Number of posts must be between 1 and 25"
 	end
 
-	if category != 'rising' || category != 'hot' || category != 'new' || category != 'controversial'
+	# The only categories of reddit (I believe)
+	if category != 'rising' && category != 'hot' && category != 'new' && category != 'controversial'
 		_event.respond "Invalid Input!"
 	end
 
@@ -86,7 +102,8 @@ bot.command(:reddit, min_args: 3, max_args: 3) do |_event, *args|
 	end
 
 	# Opens reddit url
-	html = open(url, 'User-Agent' => 'Discord Bot by u/QuantumBogo').read
+	# Uses user agent in order to make calls
+	html = open(url, 'User-Agent' => 'Discord Bot by u/input username').read
 	response = JSON.parse(html)
 
 	response = response["data"]["children"]
@@ -95,9 +112,12 @@ bot.command(:reddit, min_args: 3, max_args: 3) do |_event, *args|
 	urls = []
 
 	response.each do |child|
+
+		# Grabs title of post and it's data type (selftext or link)
 		titles.push(child["data"]["title"])
 		selftext = child["data"]["selftext"]
 
+		# Returns url if not selftext post
 		if selftext == ''
 			urls.push(child["data"]["url"])
 		else
@@ -119,12 +139,14 @@ bot.command(:reddit, min_args: 3, max_args: 3) do |_event, *args|
 
 end
 
+# Random number generator
 bot.command :roll do |event, arg|
-	event.respond r.rand(arg.to_i)
+	r.rand(arg.to_i)
 end
 
+# Random Coin flip, wierd bug, working on a fix
 bot.command :coin do |event|
-	coin = r.rand(1)
+	coin = r.rand(2)
 
 	event << ''
 
@@ -135,17 +157,135 @@ bot.command :coin do |event|
 	end
 end
 
+# Anime list searcher
+bot.command :anime do |event, *args|
+
+	# Sets up link for API
+	anime = ''
+	args.each do |str|
+		anime = anime + str + '+'
+	end
+	anime = anime[0...-1]
+	anime.gsub(' ', '+')
+	url = "http://myanimelist.net/api/anime/search.xml?q=" + anime
+
+	# Grabs data using XML parser Nokogiri
+	# Requires My Anime List account to access the api
+	doc = Nokogiri::XML(open(url, http_basic_authentication: ["MAL username", "MAL password"]))
+
+	# Parses through all data of first entry (Some have multiple entries)
+	doc.xpath('//entry').each do |entry|
+
+		event.respond entry.xpath('image').text
+
+		response = "\n\n" + entry.xpath('title').text + "\n" + entry.xpath('english').text + "\n\n"
+		response = response + "Score : " + entry.xpath('score').text + "\n\n"
+		response = response + "Type : " + entry.xpath('type').text + "\n\n"
+		response = response + "Episodes : " + entry.xpath('episodes').text + "\n\n"
+		response = response + "Status : " + entry.xpath('status').text + "\n\n"
+		response = response + "Aired : " + entry.xpath('start_date').text + " - " + entry.xpath('end_date').text + "\n\n"
+		response = response + "Synopsis : \n" + entry.xpath('synopsis').text.gsub('<br />', "")
+
+		event.respond response
+		break
+	end
+
+end
+
+# Manga API information bot
+bot.command :manga do |event, *args|
+
+	# Sets up URL
+	manga = ''
+	args.each do |str|
+		manga = manga + str + '+'
+	end
+	manga = manga[0...-1]
+	manga.gsub(' ', '+')
+	url = "http://myanimelist.net/api/manga/search.xml?q=" + manga
+
+	# Uses Nokogiri to parse XML 
+	# Requires My Anime List account to access the api
+	doc = Nokogiri::XML(open(url, http_basic_authentication: ["MAL username", "MAL password"]))
+
+	# Grabs all data from first entry
+	doc.xpath('//entry').each do |entry|
+
+		event.respond entry.xpath('image').text
+
+		response = "\n\n" + entry.xpath('title').text + "\n" + entry.xpath('english').text + "\n\n"
+		response = response + "Score : " + entry.xpath('score').text + "\n\n"
+		response = response + "Chapters : " + entry.xpath('chapters').text + "\n\n"
+		response = response + "Volumes : " + entry.xpath('volumes').text + "\n\n"
+		response = response + "Status : " + entry.xpath('status').text + "\n\n"
+
+		end_date = entry.xpath('end_date').text
+
+		# If manga is ongoing then the date will be 0000-00-00
+		if end_date == "0000-00-00"
+			end_date = "On-Going"
+		end
+
+		response = response + "Aired : " + entry.xpath('start_date').text + " - " + end_date + "\n\n"
+		response = response + "Synopsis : \n" + entry.xpath('synopsis').text.gsub('<br />', "")
+
+		event.respond response
+		break
+	end
+
+end
+
+# Champion data scraper from champion.gg
+bot.command :stats do |event, *args|
+
+	# Name may have spaces
+	counter = 0
+	champion = ''
+	while counter < args.size() - 1
+		champion = champion + args[counter]
+		counter = counter + 1
+	end
+
+	if champion == 'wukong'
+		champion = 'monkeyking'
+	end
+
+	role = args[-1]
+
+	# ADC and Middle has a few different representations
+	if role == 'marksman' || role == 'bot'
+		role = "adc"
+	elsif role == 'mid'
+		role = "middle"
+	end
+
+	url = "http://champion.gg/champion/" + champion + "/" + role
+
+	# Nokogiri html parser
+	doc = Nokogiri::HTML(open(url, "User-Agent" => "Discord Bot"))
+
+	event << ""
+	event << doc.css("li[class='selected-role'] h3").text.strip
+	event << doc.css("li[class='selected-role']  small")[0].text.strip + " Out of" + doc.css("li[class='selected-role'] small")[1].text
+	event << "Win-Rate : " + doc.css("tr[id='statistics-win-rate-row'] td")[1].text
+	event << "Play-Rate : " + doc.css("tr[id='statistics-play-rate-row'] td")[1].text 
+
+end
+
+# just tells users about different commands
 bot.command :commands do |event|
 	event << ""
-	event << "!randomroles [names]  - Given between 2 to 5 names it chooses random league roles for them"
 	event << "!hi  - Gives you a friendly greeting ^ ^"
-	event << "!randomchampion [number]  - Gives you a number of random champions"
-	event << "!roll [number]  - Gives you a random number between given number"
+	event << "!randomroles [Names]  - Given between 2 to 5 names it chooses random league roles for them"
+	event << "!randomchampion [Number  between 1 - " + champs.size().to_s + "]  - Gives you a number of random champions"
+	event << "!roll [Number]  - Gives you a random number between given number"
 	event << "!coin  - Returns heads or tails"
-	event << "!reddit [subreddit, category, number]  - Givens certain number of posts on subreddit by category \n\n"
+	event << "!stats [Champion, Role] - Returns Stats for champion in given role, if no data from that role, defaults to most played role"
+	event << "!reddit [Subreddit, Category, Number]  - Givens certain number of posts on subreddit by category"
+	event << "!anime [Anime]  - Gives information for the given anime"
+	event << "!manga [Manga]  - Gives information for the given manga \n\n"
 	event << "Jonah is the best bot creator"
 end
 
 bot.run
-
 
